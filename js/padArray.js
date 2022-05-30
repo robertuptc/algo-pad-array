72a44f710412c9f56b15b293396e3c0abfb7a581
robertuptc <robertuptc@gmail.com>
2022-05-30 12:07:26 -0400

    finished exercises

--- js/padArray.js
+++ js/padArray.js
@@ -1,4 +1,13 @@
 // REMEMBER TO PSEUDOCODE
 const pad = (array, minSize, value=null) => {
-
+    let newArray = array.slice()
+    
+    while (newArray.length < minSize + 1) {
+        newArray.push(value)
+        minSize -= 1 
+    }
+   
+    return newArray
 }
+
+
--- python/pad_array.py
+++ python/pad_array.py
@@ -1,2 +1,13 @@
 #REMEMBER TO PSEUDOCODE
 def pad(array, min_size, value = None):
+    new_list = array
+
+    while len(new_list) < min_size:
+        new_list.append(value)
+        min_size -= 1
+    
+    return new_list
+
+
+
+
